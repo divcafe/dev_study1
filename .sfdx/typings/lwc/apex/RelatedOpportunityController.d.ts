09d86bf28653bb3445eca6587bc35f446aea61b3
divecafe <thanks@divecafe.org>
2026-02-04 22:33:06 +0900

    日付変更

---
+++ .sfdx/typings/lwc/apex/RelatedOpportunityController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/RelatedOpportunityController.getOpportunities" {
+  export default function getOpportunities(param: {accountId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/RelatedOpportunityController.updateOpportunities" {
+  export default function updateOpportunities(param: {data: any}): Promise<any>;
+}
